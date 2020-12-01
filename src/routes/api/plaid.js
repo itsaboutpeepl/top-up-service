1081fffeeb2afa1caeb4e5a8e5cdcf53a5255e35
Lior Agnin <lior.agnin@gmail.com>
2020-12-01 15:34:46 +0200

    fix

--- src/routes/api/plaid.js
+++ src/routes/api/plaid.js
@@ -58,8 +58,6 @@ router.post('/create_link_token_for_payment', async (req, res) => {
       user: {
         client_user_id: user.id
       },
-      client_id: config.get('plaid.clientID'),
-      secret: config.get('plaid.secret'),
       client_name: 'Peepl wallet',
       country_codes,
       language,
