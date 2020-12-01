bda3f8512ac93907451c07ea6e883752fdb8ac63
Lior Agnin <lior.agnin@gmail.com>
2020-12-01 15:40:04 +0200

    fix useNewUrlParser

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ const init = async () => {
   mongoose.set('useFindAndModify', false)
   mongoose.set('useCreateIndex', true)
 
-  mongoose.connect(config.get('mongo.uri')).catch((error) => {
+  mongoose.connect(config.get('mongo.uri'), { useNewUrlParser: true }).catch((error) => {
     console.error(error)
     process.exit(1)
   })
