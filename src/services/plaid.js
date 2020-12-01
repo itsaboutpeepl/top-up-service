fd8ea671b57f1ba9fee96d88f38d27ef96e220a6
Lior Agnin <lior.agnin@gmail.com>
2020-12-01 15:45:38 +0200

    added logs

--- src/services/plaid.js
+++ src/services/plaid.js
@@ -1,6 +1,9 @@
 const plaid = require('plaid')
 const config = require('config')
 
+console.log({ plaidSecret: config.get('plaid.secret') })
+console.log({ plaidClientId: config.get('plaid.clientID') })
+
 const plaidClient = new plaid.Client({
   clientID: config.get('plaid.clientID'),
   secret: config.get('plaid.secret'),
