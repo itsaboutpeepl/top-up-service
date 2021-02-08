08891c0f6de4775100e4b6e03a10d98d603775dd
Lior Agnin <lior.agnin@gmail.com>
2021-02-08 12:21:58 +0200

    remove comments

--- src/routes/api/stripe.js
+++ src/routes/api/stripe.js
@@ -5,24 +5,19 @@ const { stripeClient } = require('@services/stripe')
 const { mintTokensAndSendToken, generateCorrelationId } = require('@utils/fuseApi')
 
 const generateResponse = intent => {
-  // Generate a response based on the intent's status
   switch (intent.status) {
     case 'requires_action':
     case 'requires_source_action':
-      // Card requires authentication
       return {
         requiresAction: true,
         clientSecret: intent.client_secret
       }
     case 'requires_payment_method':
     case 'requires_source':
-      // Card was not properly authenticated, suggest a new payment method
       return {
         error: 'Your card was denied, please provide a new payment method'
       }
     case 'succeeded':
-      // Payment is complete, authentication not required
-      // To cancel the payment after capture you will need to issue a Refund (https://stripe.com/docs/api/refunds)
       console.log('💰 Payment received!')
       return { clientSecret: intent.client_secret, requiresAction: false }
   }
@@ -66,8 +61,6 @@ router.post('/webhook', async (req, res) => {
     data = event.data.object
     eventType = event.type
   } else {
-    // Webhook signing is recommended, but if the secret is not configured in `config.js`,
-    // we can retrieve the event data directly from the request body.
     data = req.body.data
     eventType = req.body.type
   }
